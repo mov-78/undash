c2d63214d889e084d5ba701481b4f07a3681be87
Pwn <hi@pwn.buzz>
2016-07-19 19:10:37 +0800

    添加 curry 方法

--- undash.js
+++ undash.js
@@ -14,10 +14,21 @@
   var _ = { VERSION : '0.0.1' }
   var old = { _ : root._ , U : root.U }
 
+  var slice = Array.prototype.slice
+
   _.noConflict = function noConflict( deep ) {
     root._ = old._
     deep && ( root.U = old.U )
     return _
+  }
+
+  _.curry = function curry( fn , arity , args ) {
+    args = args || []
+    typeof arity == 'number' || ( arity = fn.length )
+    return function wrapper() {
+      args = args.concat( slice.call( arguments ) )
+      return args.length < arity ? curry( fn , arity , args ) : fn.apply( this , args )
+    }
   }
 
   return _
