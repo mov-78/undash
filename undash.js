81b22df48b61ec5469311928897108c6f5c149a7
Pwn <hi@pwn.buzz>
2016-08-16 18:05:18 +0800

    添加 tag 方法

--- undash.js
+++ undash.js
@@ -12,12 +12,30 @@
     var _ = { VERSION : '0.0.1' }
     var prev = { _ : root._ , U : root.U }
 
+    var toString = Object.prototype.toString
+
     _.noConflict = function noConflict( deep ) {
         root._ = prev._
         if ( deep ) {
             root.U = prev.U
         }
         return _
+    }
+
+    _.tag = function tag( value ) {
+
+        var _tag
+
+        if ( value === null ) {
+            return 'null'
+        }
+        if ( value === void 0 ) {
+            return 'undefined'
+        }
+
+        _tag = toString.call( value )
+        return _tag.substring( 8 , _tag.length - 1 ).toLowerCase()
+
     }
 
     return _
