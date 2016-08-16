c640c1a32da7b131c2f14b850f61991e62d71688
Pwn <hi@pwn.buzz>
2016-08-16 15:55:19 +0800

    更新代码风格

--- undash.js
+++ undash.js
@@ -1,36 +1,25 @@
-
 ( function ( root , factory ) {
-
-  if ( typeof define === 'function' && define.amd ) {
-    define( 'undash' , function () {
-      return root._ = root.U = factory( root ) // eslint-disable-line no-return-assign
-    } )
-  } else {
-    root._ = root.U = factory( root )
-  }
-
+    if ( typeof define === 'function' && define.amd ) {
+        define( function () {
+            return root._ = root.U = factory( root ) // eslint-disable-line no-return-assign
+        } )
+    } else {
+        root._ = root.U = factory( root )
+    }
 } )( this , function ( root ) {
 
-  var _ = { VERSION : '0.0.1' }
-  var old = { _ : root._ , U : root.U }
 
-  var slice = Array.prototype.slice
+    var _ = { VERSION : '0.0.1' }
+    var prev = { _ : root._ , U : root.U }
 
-  _.noConflict = function noConflict( deep ) {
-    root._ = old._
-    deep && ( root.U = old.U )
-    return _
-  }
-
-  _.curry = function curry( fn , arity , args ) {
-    args = args || []
-    typeof arity == 'number' || ( arity = fn.length )
-    return function wrapper() {
-      args = args.concat( slice.call( arguments ) )
-      return args.length < arity ? curry( fn , arity , args ) : fn.apply( this , args )
+    _.noConflict = function noConflict( deep ) {
+        root._ = prev._
+        if ( deep ) {
+            root.U = prev.U
+        }
+        return _
     }
-  }
 
-  return _
+    return _
 
 } )
